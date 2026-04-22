77af8e33434b925cbf537ac9fa80424a3df97ce1
Linus Dannull <d.linus@iht32-1502.engr.wustl.edu>
2026-04-21 23:25:29 -0500

    remove anomaly

--- spark_code.py
+++ spark_code.py
@@ -260,7 +260,7 @@ def build_stream(spark, out_path, ckpt_path):
     raw_df = parsed_df
     raw_df = raw_df.withColumn("event_dt", to_date("event_ts"))
 
-    alerts_df = result_df.filter(col("is_anomaly") == True)
+    alerts_df = result_df
     alerts_df = alerts_df.withColumn("event_dt", to_date("event_ts"))
 
     # -----------------------------------
